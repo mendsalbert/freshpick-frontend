2a05d37808197733218a0bb98cee352ffe925d48
mendsalbert <mendsalbert@gmail.com>
2022-10-25 22:42:49 +0000

    freshpick

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./Redux/store";
 import { Provider } from "react-redux";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://freshpick-server.herokuapp.com";
 
 ReactDOM.render(
   <Provider store={store}>
